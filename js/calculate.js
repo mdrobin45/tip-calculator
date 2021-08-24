400cf807af2da440f7fbaf399080349cec832f6b
Md Robin Rana <robin4500bd@gmail.com>
2021-08-24 08:25:07 +0600

    calculation complete

---
+++ js/calculate.js
@@ -0,0 +1,22 @@
+function getInputValue(inputId) {
+    const value = document.getElementById(inputId).value;
+    const toNumberValue = Number(value);
+    return toNumberValue;
+}
+document.getElementById('keyPad').addEventListener('click', function (event) {
+    const billed = getInputValue('billedAmount');
+    const person = getInputValue('numberOfPerson');
+    const tip = event.target.value;
+    if (tip != undefined && isNaN(tip) == true) {
+        const toNumber = parseFloat(tip);
+        const percent = toNumber / 100;
+        const percentAmount = percent * billed;
+        const billedWithPercent = billed + percentAmount;
+        const tipPerPerson = percentAmount / person;
+        const PerPersonAmount = billedWithPercent / person;
+        const tipAmountPerPerson = document.getElementById('tipAmount');
+        tipAmountPerPerson.innerText = tipPerPerson.toFixed(2);
+        const totalAmountPerPerson = document.getElementById('totalAmount');
+        totalAmountPerPerson.innerText = PerPersonAmount.toFixed(2);
+    }
+});
